d1efe6fc6384f8d46ef02eac82a0cd6018000c8c
Ryojidev <ryojikitanodev@gmail.com>
2020-03-03 21:33:16 -0300

    Controller

---
+++ VemDeZap/VemDeZap.Api/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VemDeZap.Domain.Commands.User.AddUser;
+
+namespace VemDeZap.Api.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IMediator _mediator;
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("Adicionar")]
+        public async Task<IActionResult> Adicionar([FromBody]RequestAddUser request)
+        {
+            try
+            {
+                var response = await _mediator Send(request, CancellationToken.None);
+                return await ResponseAsync(response);
+            }catch(System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
